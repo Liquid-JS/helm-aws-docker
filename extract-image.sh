a37faf8847b5b6ebe2e64d7da5bb38624157849d
HitkoDev <development@hitko.si>
2021-06-02 14:25:17 +0200

    strip prefix path on extract

--- extract-image.sh
+++ extract-image.sh
@@ -6,11 +6,12 @@ cd $4
 
 skopeo copy --src-creds=$1 docker://$2 dir:tmp
 layers=$(jq -r '.layers[] | .digest[7:]' tmp/manifest.json)
+dirs=$(echo $3 | tr "/" "\n" | wc -w)
 
 for l in $layers
 do
     mv tmp/$l tmp/$l.tar.gz
-    tar -xvf tmp/$l.tar.gz $3 
+    tar --strip-components=$dirs -xvf tmp/$l.tar.gz $3
 done
 
 rm -rf tmp
